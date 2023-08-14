1680ef718d5513b6ef758911c4a3434316a863d8
mame7777 <86618354+mame7777@users.noreply.github.com>
2023-08-15 00:20:23 +0900

    [fix]

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import * as React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <p>This is blog of mame77.</p>
+      <p>mame77のブログへようこそ!</p>
+      <p>旧ブログシステムが故障したため，新規構築中です！</p>
+    </div>
+  );
 }
