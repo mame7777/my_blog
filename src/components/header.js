ab2e385705f47023971afe56a163492dddc286f5
mame7777 <86618354+mame7777@users.noreply.github.com>
2023-08-16 01:58:25 +0900

    [add] ナビゲーションバー導入

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
   const data = useStaticQuery(
@@ -13,18 +14,29 @@ const Header = () => {
       }
     `
   );
+
   return (
     <header>
-      <p>{ data.site.siteMetadata.title }</p>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">top</Link>
-          </li>
-          <li>
-            <Link to="/about">about</Link>
-          </li>
-        </ul>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">{data.site.siteMetadata.title}</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse ms-4" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link active" to="/about">About</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link disabled" to="/posts">Posts</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
     </header>
   );
