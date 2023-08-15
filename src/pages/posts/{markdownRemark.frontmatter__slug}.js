cf0b40a686a1b65134e556c99cf884e2d11b3770
mame7777 <86618354+mame7777@users.noreply.github.com>
2023-08-15 22:00:52 +0900

    [add] FileSystem Route APIが動いた！

--- src/pages/posts/{markdownRemark.frontmatter__slug}.js
+++ src/pages/posts/{markdownRemark.frontmatter__slug}.js
@@ -1,5 +1,5 @@
-import * as React from 'react';
 import { graphql } from 'gatsby';
+import * as React from 'react';
 import Layout from '../../components/layout';
 
 export default function BlogPostTemplate({ data: { markdownRemark } }) {
@@ -24,4 +24,3 @@ export const query = graphql`
     }
   }
 `;
-
