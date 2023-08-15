52a79f53329686111e9edf3e561b4fa98a8d72bd
mame7777 <86618354+mame7777@users.noreply.github.com>
2023-08-15 21:36:39 +0900

    [add] slugを追加した．（動かない）

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,40 @@
 import * as React from "react"
 import Layout from '../components/layout';
+import { graphql } from 'gatsby';
 
-export default function Home() {
+const IndexPage = ({ data }) => {
   return (
     <Layout>
       <h1>This is blog of mame77.</h1>
       <p>mame77のブログへようこそ!</p>
       <p>旧ブログシステムが故障したため，新規構築中です！</p>
+
+      {data.allMarkdownRemark.edges.map((edge) => (
+        <div key={edge.node.id}>
+          <h2>{edge.node.frontmatter.title}</h2>
+          <p>{edge.node.frontmatter.date}</p>
+          <div dangerouslySetInnerHTML={{ __html: edge.node.html }} />
+        </div>
+      ))}
     </Layout>
   );
 }
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          html
+          timeToRead
+          frontmatter {
+            title
+            date
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default IndexPage;
---
+++ src/posts/{markdownRemark.frontmatter__slug}.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { graphql } from 'gatsby';
+import Layout from '../../components/layout';
+
+export default function BlogPostTemplate({ data: { markdownRemark } }) {
+  const { frontmatter, html } = markdownRemark;
+  return (
+    <Layout>
+      <h2>{frontmatter.title}</h2>
+      <div>{frontmatter.date}</div>
+      <div className="post-body" dangerouslySetInnerHTML={{ __html: html }} />
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query ($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+      }
+    }
+  }
+`;
+
