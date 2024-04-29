ca35dca3b678ff286f63c2d9eda6ea2e91fb642e
mame7777 <86618354+mame7777@users.noreply.github.com>
2024-04-29 21:56:09 +0900

    [fix] post card

--- src/components/postCard.js
+++ src/components/postCard.js
@@ -5,7 +5,7 @@ import { getImage, GatsbyImage } from 'gatsby-plugin-image';
 const PostCard = ({ post }) => (
   <div className="card mb-3 mt-3">
     <div className="row g-0">
-      <div className="col-md-4 align-self-center">
+      <div className="col-md-4 align-self-center text-center">
         <GatsbyImage image={getImage(post.frontmatter.hero_image)} style={{ maxHeight: "200px" }} className="img-fluid rounded-start" alt="Post card image"/>
       </div>
       <div className="col-md-8 d-flex flex-column">
