8683b017472fc22c5f6ae39c40c47e63ce1c129a
morita <86618354+mame7777@users.noreply.github.com>
2024-06-06 14:07:38 +0900

    [add] プライバシーポリシーなど追加

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,10 @@ import * as React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <p className='text-center mt-5'>© 気ままな備忘録 {new Date().getFullYear()}</p>
+      <div className="text-center  mt-5">
+      <a href="/privacy-policy">プライバシーポリシー・免責事項ほか</a>
+      <p className='text-center'>© 気ままな備忘録 2022-{new Date().getFullYear()}</p>
+      </div>
     </footer>
   );
 };
